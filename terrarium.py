779307f1ae719ec9b0fdf49f689108a164337b86
Drew Ferrington <127445538+DrewBF03@users.noreply.github.com>
2023-03-29 22:25:52 -0500

    Add files via upload

---
+++ terrarium.py
@@ -0,0 +1,89 @@
+################################################
+############# TERRARIUM CODE ###################
+################################################
+
+# GUI/MONITER #
+################################################
+
+#!/usr/bin/python3
+
+# monitor.py - For Terrarium Controllers using Adafruit
+# DHT sensors, Energenie Pimote sockets, and ThingSpeak.
+# MIT license.
+# https://www.carnivorousplants.co.uk/resources/raspberry-pi-terrarium-controller/
+
+# Imports
+from gpiozero import Energenie
+import Adafruit_DHT
+import requests
+
+# Attempt to get a sensor reading. The read_retry method will
+# retry up to 15 times, waiting 2 seconds between attempts
+sensormodel = Adafruit_DHT.AM2302
+sensorpin = 4
+humidity, temperature = Adafruit_DHT.read_retry(sensormodel, sensorpin)
+
+# If either reading has failed after repeated retries,
+# abort and log message to ThingSpeak
+thingspeak_key = 'XXXXXXXXXXXXXXXX'
+if humidity is None or temperature is None:
+	f = requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingspeak_key, 'status':'failed to get reading'})
+
+# Otherwise, check if temperature is above threshold,
+# and if so, activate Energenie socket for cooling fan
+else:
+	fansocket = 1
+	tempthreshold = 28
+
+	if temperature > tempthreshold:
+		# Activate cooling fans
+		f = Energenie(fansocket, initial_value=True)
+
+	else:
+		# Deactivate cooling fans
+		f = Energenie(fansocket, initial_value=False)
+
+	# Send the data to Thingspeak
+	r = requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingspeak_key, 'field1':temperature, 'field2':humidity})
+	
+################################################
+################################################
+
+# LIGHTS ON #
+################################################
+
+#!/usr/bin/python3
+
+# lights-on.py - For Terrarium Controllers using Adafruit
+# DHT sensors, Energenie Pimote sockets, and ThingSpeak.
+# MIT license.
+# https://www.carnivorousplants.co.uk/resources/raspberry-pi-terrarium-controller/
+
+# Imports
+from gpiozero import Energenie
+
+# Main lights off
+lightsocket = 2
+l = Energenie(lightsocket, initial_value=True)
+
+################################################
+################################################
+
+# LIGHTS OFF #
+################################################
+#!/usr/bin/python3
+
+# lights-off.py - For Terrarium Controllers using Adafruit
+# DHT sensors, Energenie Pimote sockets, and ThingSpeak.
+# MIT license.
+# https://www.carnivorousplants.co.uk/resources/raspberry-pi-terrarium-controller/
+
+# Imports
+from gpiozero import Energenie
+
+# Main lights off
+lightsocket = 2
+l = Energenie(lightsocket, initial_value=False)
+
+################################################
+################################################
